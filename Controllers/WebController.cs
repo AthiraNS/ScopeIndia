a1406738fd5b2a83040ba2c7ee97075763c5277d
nanda <nanda@ADHI-NANDHU-BIK>
2024-02-08 13:15:17 +0530

    Add project files.

---
+++ Controllers/WebController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScopeIndia.Controllers
+{
+    public class WebController : Controller
+    {
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult StudentDashboard()
+        {
+            return View();
+        }
+    }
+}
+
+
+
